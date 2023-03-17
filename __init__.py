a93f6c779cf317f5e8e97584741b7b9bca62ab05
Yiğit GÜMÜŞ <rawns0909@gmail.com>
2023-03-17 11:51:51 +0300

    push codes

---
+++ __init__.py
@@ -0,0 +1,3 @@
+from .info import *
+
+__version__ = 0.0.1
--- main.py
+++ main.py
@@ -1,184 +1,24 @@
 import webbrowser
 import random
+from info import (
+    list_of_distros,
+    list_of_eol_distros
+)
 
-distro_list = [
-    "https://ubuntu.com/",
-    "http://ubuntuce.com/",
-    "https://pop.system76.com/",
-    "https://www.kali.org/",
-    "https://tails.boum.org/",
-    "https://www.debian.org/",
-    "https://archlinux.org/",
-    "https://www.gentoo.org/",
-    "https://linuxmint.com/",
-    "https://getfedora.org/",
-    "https://developers.redhat.com/products/rhel/download",
-    "https://os.gnome.org/",
-    "https://manjaro.org/",
-    "https://chromeenterprise.google/os/chromeosflex/",
-    "https://xubuntu.org/",
-    "https://kubuntu.org/",
-    "https://www.raspberrypi.org/software/",
-    "https://store.steampowered.com/steamos",
-    "https://www.centos.org/",
-    "https://www.opensuse.org/",
-    "https://www.openmandriva.org/",
-    "https://www.clearos.com/",
-    "https://www.oracle.com/linux/",
-    "https://scientificlinux.org/",
-    "https://www.qubes-os.org/",
-    "https://geckolinux.github.io/",
-    "https://www.mageia.org/en/",
-    "https://www.rosalinux.ru/en/home-2/",
-    "https://www.pclinuxos.com/",
-    "https://en.altlinux.org/Downloads",
-    "https://www.pureos.net/",
-    "https://astralinux.ru/en/",
-    "https://www.parrotsec.org/",
-    "https://lubuntu.me/",
-    "https://ubuntubudgie.org/",
-    "https://ubuntu-mate.org/",
-    "https://ubuntustudio.org/",
-    "https://www.edubuntu.org/",
-    "https://www.backbox.org/",
-    "https://zorinos.com/",
-    "https://galliumos.org/",
-    "https://elementary.io/",
-    "https://ubuntuunity.org/",
-    "https://emmabuntus.org/category/english/",
-    "https://neon.kde.org/",
-    "https://www.linuxliteos.com/",
-    "http://www.linuxmce.org/",
-    "https://www.bodhilinux.com/",
-    "https://www.blue-systems.com/products/software/netrunner/",
-    "https://www.gooroom.kr/",
-    "https://trisquel.info/",
-    "https://peppermintos.com/",
-    "https://www.slax.org/",
-    "https://solydxk.com/",
-    "https://antixlinux.com/",
-    "https://mxlinux.org/",
-    "https://bosslinux.in/",
-    "https://www.primeos.in/download/",
-    "http://www.knopper.net/knoppix/index-en.html",
-    "https://www.devuan.org/",
-    "https://www.deepin.org/en/",
-    "https://www.crunchbangplusplus.org/",
-    "http://www.damnsmalllinux.org/",
-    "https://archbang.org/",
-    "https://artixlinux.org/",
-    "https://archlabslinux.com/",
-    "https://www.blackarch.org/",
-    "https://endeavouros.com/",
-    "https://www.frugalware.org/",
-    "https://www.hyperbola.info/",
-    "https://www.calculate-linux.org/",
-    "https://www.chromium.org/chromium-os",
-    "https://pentoo.ch/",
-    "https://www.sabayon.org/",
-    "https://nimblex.net/",
-    "https://salixos.org/",
-    "https://www.supergamer.x10host.com/",
-    "http://topologi-linux.sourceforge.net/",
-    "http://www.porteus.org/",
-    "https://www.dnalinux.com/",
-    "http://www.zenwalk.org/",
-    "http://www.lunar-linux.org/",
-    "https://guix.gnu.org/",
-    "https://www.foresightlinux.org/",
-    "https://www.gobolinux.org/",
-    "https://www.hpcfactor.com/reviews/software/jlime/donkey-1-0-2/",
-    "https://voidlinux.org/",
-    "http://www.toms.net/rb/",
-    "http://www.tinycorelinux.net/",
-    "https://getsol.us/home/",
-    "https://slitaz.org/en/",
-    "https://puppylinux.com/",
-    "https://openwrt.org/",
-    "https://nixos.org/",
-    "https://www.mklinux.org/",
-    "http://www.dragora.org/en/index.html",
-    "https://crux.nu/",
-    "https://www.alpinelinux.org/",
-    "https://armbian.com",
-    "https://qntm.org/suicide",
-    "http://www.slackware.com/",
-    "https://www.parabola.nu/",
-    "https://bedrocklinux.org/",
-    "https://instantos.io",
-    "https://maboxlinux.org/",
-    "https://ferenos.weebly.com/",
-    "https://rebornos.org/",
-    "https://endlessos.com/home/",
-    "https://www.elivecd.org/",
-    "https://q4os.org/index.html",
-    "https://reactos.org/",
-    "https://garudalinux.org/",
-    "https://www.system-rescue.org/Download/",
-    "https://kaosx.us/",
-    # who ever made this website really needs improve their image quality and stretch resolution :/
-    "https://voyagerlive.org/",
-    "https://www.bunsenlabs.org/",
-    "https://lxle.net/",
-    "https://tos.odex.be/",
-    "https://thinstation.github.io/thinstation/",
-    # not only is this distro having issues with security for it's website, the funding ended. Might move to EOL so I would have to keep an eye on them
-    "https://spi.dod.mil/lipose.htm",
-    # Rebecca Black operating system
-    "https://sourceforge.net/projects/rebeccablackos/",
-    "https://caixamagica.pt/en/linux-cm",
-    "https://easynas.org/",
-    "https://arcolinux.com/",
-    "https://arcolinuxb.com/",  # this linux has two different websites
-    "https://regolith-linux.org/",
-    "https://silverblue.fedoraproject.org/",
-    "https://almalinux.org",
-    "https://ibuntuos.com",
-    "https://nxos.org/",  # System-d free Debian based
-    "https://ultramarine-linux.org",
-    "https://cutefish-ubuntu.github.io/",
-    "https://www.ghostbsd.org/",
-    "https://www.freebsd.org/",
-    "https://www.openbsd.org/",
-    "https://www.netbsd.org/",
-    "https://www.dragonflybsd.org/",
-    "https://www.midnightbsd.org/",
-    "https://nomadbsd.org/",
-    "https://hellosystem.github.io/",
-    "https://rockylinux.org/",
-    "https://www.mocaccino.org",
-    "https://spirallinux.github.io/",  # Created by Gecko Linux's anonymous author
-    "https://ponyos.org/",  # Completely forgot about this
-    "http://collapseos.org/",
-    "https://nobaraproject.org/",
-    "https://linuxkamarada.com/",
-    "https://berry-lab.net/",
-    "https://sourceforge.net/projects/xange/",  # Kedora
-    "https://img.cs.montana.edu/linux/montanalinux/",  # Montana Linux
-    "https://www.regataos.com",  # Based on OpenSuse Leap
-    "https://archhurd.org/",  # Arch based on GNUHurd
-    "https://www.freenix.net/",  # Slackware with non-free packages
-    "https://ravynos.com/",
-    "https://uwuntuos.site/",  # uwu
-    "https://www.miraclelinux.com/",
-    "https://getcryst.al/site",  # Arch distro with GNOME and Amethyst AUR helper
-    # Based on Bodhi, designed for preschool
-    "https://escuelaslinux.sourceforge.io/english/index.html",
-    "https://liguros.gitlab.io/",  # Gentoo based
-    "https://pikaos.ferreo.dev/",  # Gaming Ubuntu based distro with Nobara Tweaks
-    "https://redcorelinux.org/",  # Hardened Gentoo
-    "https://pearos.xyz/",  # macos like Arch
-    "https://chimera-linux.org/",
-    "https://venomlinux.org/",
-    "https://kisslinux.org/",
-    # anti-forensic and anonymous distro based on Debian
-    "https://www.digi77.com/linux-kodachi/",
-    "https://blends.debian.org/edu/",
-    "https://easyos.org/",  # Experimental Linux distribution made by Puppy Linux's maintainer
-    "https://www.plamolinux.org/",  # Slackware-based
-    # Debian-based Japanese distro meant for business support through licensing
-    "http://www.omoikane.co.jp/",
-    "https://fuguita.org/"  # OpenBSD-based live system
-]
+class DistroChooser:
+    global distro_list, eol_distro_list
+    distro_list = list_of_distros
+    eol_distro_list = list_of_eol_distros
+            
+    @staticmethod
+    def choose() -> str:
+        are_you_want_eol = input('Do you want EOL Distros: ')
 
-webbrowser.open(distro_list[random.randint(0, len(distro_list) - 1)], new = 2)
+        if "yes" in are_you_want_eol:
+            distro_list.append(eol_distro_list)
+
+        return distro_list[random.randint(0, len(distro_list) - 1)]
+
+
+
+webbrowser.open(DistroChooser.choose(), new = 2)
